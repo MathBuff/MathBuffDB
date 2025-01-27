c334b6cd4c808fe19762d2aaf5f4e6c871bda9c7
root <root@BillyUnit.>
2025-01-27 01:11:08 -0600

    quickSave

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,26 @@ int main () {
 	//TextPort KermanTable("data/names.txt","read");
 	//KermanTable.printCurrentRow();
 	
+	/*
 	TextPort letter("data/letter.txt","write");
 	letter.coutToCurrentRow("Dear Putin,\n");
 	letter.coutToCurrentRow("You have been a very bad man.\n");
 	letter.coutToCurrentRow("It's about time you fucking stop the war you dumb bitch\n");
 	letter.coutToCurrentRow("Your's Truely,\n");
 	letter.coutToCurrentRow("Stalin <3\n");
+	*/
+	
+	/*
+	TextPort letter("data/letter.txt","read");
+	letter.printCurrentRow();
+	letter.printCurrentRow();
+	letter.printCurrentRow();
+	letter.printCurrentRow();
+	letter.printCurrentRow();
+	*/
+	
+	//TextPort letter("data/letter.txt","write");
+	
 	std::cout<<std::endl;
  
  return 0; 
