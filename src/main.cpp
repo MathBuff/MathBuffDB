a488cff01d58349c0919f53cb82f1e3997ab7df6
root <root@BillyUnit.>
2025-01-26 22:59:51 -0600

    quickSave

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
-template <class T>
-constexpr T speed_of_light = T(299'792'458);
-
-int main(){
-std::cout << speed_of_light <double> << '\n';
-std::cout << speed_of_light <float> << '\n';
-std::cout << speed_of_light <int> << '\n';
-std::cout << speed_of_light <long> << '\n';
+int main () {
+ 
+ return 0; 
 }
